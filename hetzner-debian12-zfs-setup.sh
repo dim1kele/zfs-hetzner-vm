80a72b6a2ba8457d7da9f12743ced7164bcc2e1a
dim1kele <155857933+dim1kele@users.noreply.github.com>
2024-01-07 02:31:37 +0100

    Update hetzner-debian12-zfs-setup.sh
    only raidz2

--- hetzner-debian12-zfs-setup.sh
+++ hetzner-debian12-zfs-setup.sh
@@ -544,12 +544,7 @@ echo "======= create zfs pools and datasets =========="
     bpool_disks_partitions+=("${selected_disk}-part2")
   done
 
-  pools_mirror_option=
-  if [[ ${#v_selected_disks[@]} -gt 1 ]]; then
-    if dialog --defaultno --yesno "Do you want to use mirror mode for ${v_selected_disks[*]}?" 30 100; then 
-      pools_mirror_option=raidz2
-    fi
-  fi
+  pools_mirror_option=raidz2
 
 # shellcheck disable=SC2086
 zpool create \
